a595c84f92512929ef1fb0e8e84e356619bda667
Paul Jolly <paul@myitcv.org.uk>
2014-09-03 19:18:06 +0100

    First cut of submanager

--- neovim_test.go
+++ neovim_test.go
@@ -11,6 +11,7 @@ import (
 	"os/exec"
 	"strings"
 	"sync"
+	"sync/atomic"
 
 	"testing"
 
@@ -101,73 +102,57 @@ func (t *NeovimTest) TestBufferGetLength(c *C) {
 }
 
 func (t *NeovimTest) TestBufferSetGetLine(c *C) {
-	b, err := t.client.GetCurrentBuffer()
-	c.Assert(err, IsNil)
+	b, _ := t.client.GetCurrentBuffer()
 	val := "This is line 1"
-	err = b.SetLine(0, val)
-	c.Assert(err, IsNil)
-	l, err := b.GetLine(0)
-	c.Assert(err, IsNil)
+	_ = b.SetLine(0, val)
+	l, _ := b.GetLine(0)
 	c.Assert(l, Equals, val)
-	err = b.DelLine(0)
-	c.Assert(err, IsNil)
-	length, err := b.GetLength()
-	c.Assert(err, IsNil)
+	_ = b.DelLine(0)
+	length, _ := b.GetLength()
 	c.Assert(length, Equals, 1)
 }
 
 func (t *NeovimTest) TestEval(c *C) {
-	res, err := t.client.Eval(`4`)
-	c.Assert(err, IsNil)
+	res, _ := t.client.Eval(`4`)
 	resInt64 := res.(int64)
 	c.Assert(resInt64 > 0, Equals, true)
 }
 
 func (t *NeovimTest) TestClientSubscribe(c *C) {
-	respChan := make(chan neovim.SubscriptionEvent)
-	errChan := make(chan error)
 	topic := "event1"
-	val := []interface{}{1, 2, 3}
+	val := []int64{1, 2, 3}
 
 	vals := make([]string, len(val))
 	for i := range val {
 		vals[i] = fmt.Sprintf("%v", val[i])
 	}
 
-	t.client.SubChan <- neovim.Subscription{
-		Topic:  topic,
-		Events: respChan,
-		Error:  errChan,
+	sub, _ := t.client.Subscribe(topic)
+	command := fmt.Sprintf(`call send_event(0, "%v", %v)`, topic, strings.Join(vals, ","))
+	_ = t.client.Command(command)
+	resp := <-sub.Events
+	c.Assert(len(val), Equals, len(resp.Value))
+	for i := range resp.Value {
+		c.Assert(resp.Value[i], Equals, val[i])
 	}
-	err := <-errChan
-	c.Assert(err, IsNil)
-	err = t.client.Subscribe(topic)
-	c.Assert(err, IsNil)
-	command := fmt.Sprintf(`call send_event(0, "%v", [%v])`, topic, strings.Join(vals, ","))
-	err = t.client.Command(command)
-	c.Assert(err, IsNil)
-	resp := <-respChan
 	c.Assert(resp, NotNil)
 }
 
 func (t *NeovimTest) TestGetSlice(c *C) {
-	cb, err := t.client.GetCurrentBuffer()
-	c.Assert(err, IsNil)
-	lines, err := cb.GetSlice(0, -1, true, true)
-	c.Assert(err, IsNil)
+	cb, _ := t.client.GetCurrentBuffer()
+	lines, _ := cb.GetSlice(0, -1, true, true)
 	c.Assert(lines, NotNil)
 }
 
 func (t *NeovimTest) TestNumberEval(c *C) {
-	_, err := t.client.Eval("127")
-	c.Assert(err, IsNil)
+	i, _ := t.client.Eval("1")
+	c.Assert(i, Equals, int64(1))
 }
 
 func (t *NeovimTest) TestArrayEval(c *C) {
-	err := t.client.Command("let x=1 | let y=2")
-	_v, err := t.client.Eval("[x,y]")
+	_ = t.client.Command("let x=1 | let y=2")
+	_v, _ := t.client.Eval("[x,y]")
 	v := _v.([]interface{})
-	c.Assert(err, IsNil)
 	comp := []int64{1, 2}
 	c.Assert(len(v), Equals, len(comp))
 	for i := range v {
@@ -177,10 +162,8 @@ func (t *NeovimTest) TestArrayEval(c *C) {
 
 func (t *NeovimTest) BenchmarkCommandAndEval(c *C) {
 	for i := 0; i < c.N; i++ {
-		err := t.client.Command(fmt.Sprintf("let x=%v", i))
-		c.Assert(err, IsNil)
-		v, err := t.client.Eval("x")
-		c.Assert(err, IsNil)
+		_ = t.client.Command(fmt.Sprintf("let x=%v", i))
+		v, _ := t.client.Eval("x")
 		switch v.(type) {
 		case int64:
 			c.Assert(v, Equals, int64(i))
@@ -194,8 +177,7 @@ func (t *NeovimTest) BenchmarkCommandAndEval(c *C) {
 
 func (t *NeovimTest) BenchmarkMatchAddEmptyBuffer(c *C) {
 	for i := 0; i < c.N; i++ {
-		id, err := t.client.Eval(fmt.Sprintf("matchadd('String', '\\%%%vl\\%%2c\\_.\\{8\\}')", i))
-		c.Assert(err, IsNil)
+		id, _ := t.client.Eval(fmt.Sprintf("matchadd('String', '\\%%%vl\\%%2c\\_.\\{8\\}')", i))
 		c.Assert(id, NotNil)
 	}
 }
@@ -207,4 +189,51 @@ func (t *NeovimTest) BenchmarkGetBufferContents(c *C) {
 	for i := 0; i < c.N; i++ {
 		_, _ = cb.GetSlice(0, -1, true, true)
 	}
+}
+
+func (t *NeovimTest) TestMultiClientSubscribe(c *C) {
+	topic := "event1"
+	var subDone, unsubDone, doneDone sync.WaitGroup
+	var check int64
+
+	number := 1000
+
+	for i := 1; i <= number; i++ {
+		subDone.Add(1)
+		if i%2 == 1 {
+			unsubDone.Add(1)
+		}
+		doneDone.Add(1)
+		go func(topic string, n int, check *int64) {
+			sub, _ := t.client.Subscribe(topic)
+			subDone.Done()
+			resp := <-sub.Events
+			val := resp.Value[0].(int64)
+			atomic.AddInt64(check, val)
+			if n%2 == 0 {
+				// listen again
+				resp := <-sub.Events
+				val := resp.Value[0].(int64)
+				atomic.AddInt64(check, val)
+			} else {
+				// unsubscribe
+				t.client.Unsubscribe(sub)
+				unsubDone.Done()
+			}
+			doneDone.Done()
+		}(topic, i, &check)
+	}
+
+	subDone.Wait()
+
+	command := fmt.Sprintf(`call send_event(0, "%v", 1)`, topic)
+	_ = t.client.Command(command)
+
+	unsubDone.Wait()
+
+	_ = t.client.Command(command)
+
+	doneDone.Wait()
+
+	c.Assert(atomic.LoadInt64(&check), Equals, int64(number+number/2))
 }
