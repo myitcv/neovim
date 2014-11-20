fd5cab5cc3333897f191bfd3c9a2702c4e173e1c
Paul Jolly <paul@myitcv.org.uk>
2014-11-20 09:50:44 +0000

    Use the nightly build

--- run_build.sh
+++ run_build.sh
@@ -3,21 +3,7 @@
 set -e
 set -x
 
-# Use the old approach to building until https://github.com/neovim/neovim/pull/1335 is merged
-# source <(curl -fsSL https://raw.githubusercontent.com/neovim/neovim/master/.ci/common.sh)
-
-# set_environment /opt/neovim-deps
-
-if [ ! -d $TRAVIS_BUILD_DIR/_neovim ]
-then
-  git clone -b provider-autocmd https://github.com/tarruda/neovim.git $TRAVIS_BUILD_DIR/_neovim
-fi
-
-pushd $TRAVIS_BUILD_DIR/_neovim
-# git fetch origin
-# git rebase origin/master
-make
-popd
+eval "$(curl -Ss https://raw.githubusercontent.com/neovim/bot-ci/master/scripts/travis-setup.sh) nightly-x64";
 
 # check that the generated API matches what we have committed
 # (this ensures that the Neovim API hasn't moved on without us
@@ -26,7 +12,7 @@ popd
 pushd $TRAVIS_BUILD_DIR/_cmd/gen_neovim_api/
 go get -d -t -v ./... && go build -v ./...
 x=`mktemp`
-NEOVIM_BIN=$TRAVIS_BUILD_DIR/_neovim/build/bin/nvim ./gen_neovim_api -g -o api.go.gen -t api_test.go.gen
+NEOVIM_BIN=$TRAVIS_BUILD_DIR/_neovim/bin/nvim ./gen_neovim_api -g -o api.go.gen -t api_test.go.gen
 cat api.go.gen | gofmt | diff -- - ../../gen_client_api.go
 if [ $? -ne 0 ]
 then
@@ -42,5 +28,5 @@ fi
 rm $x
 popd
 
-NEOVIM_BIN=$TRAVIS_BUILD_DIR/_neovim/build/bin/nvim go test
-NEOVIM_BIN=$TRAVIS_BUILD_DIR/_neovim/build/bin/nvim go test -race
+NEOVIM_BIN=$TRAVIS_BUILD_DIR/_neovim/bin/nvim go test
+NEOVIM_BIN=$TRAVIS_BUILD_DIR/_neovim/bin/nvim go test -race
